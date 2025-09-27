dc3211d72ce6ccfc8fb6fe0990d56b8cae6ba530
ArchieChadha <archiechadha12@gmail.com>
2025-09-27 15:23:44 +1000

    Fix profile image path for new repository name

--- src/components/About.js
+++ src/components/About.js
@@ -67,7 +67,7 @@ class About extends React.Component {
               {[two]}
             </div>
             <div className="about-image">
-              <img src="/archie-portfolio/images/projects/profile1.jpg" alt="Archie Chadha" />
+              <img src="/archie-chadha/images/projects/profile1.jpg" alt="Archie Chadha" />
             </div>
           </div>
         </FadeInSection>
